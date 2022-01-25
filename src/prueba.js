49f6eb0d2ad91b13e4a7ae92ff347ca2e5ba1a06
Juanjee24 <jsc0009@alu.medac.es>
2022-01-25 14:35:50 +0100

    tarea node, avanzando

--- src/prueba.js
+++ src/prueba.js
@@ -1,12 +1,11 @@
-window.onload=function(){
+function llamada(id_usu){
     var h3 = document.querySelector("h3");
     var usuario;
-    var id_usu = mostrar();
+    
     var formu = document.forms[0];
-    var input2 = formu.campo2;
-    
-    
-    
+    var inputNombre = formu.campo2;
+    var inputEmail = formu.campo3;
+    var inputTelefono = formu.campo4;
     
 
     fetch("http://localhost:3000/usuario?ID_Usuario="+id_usu, {
@@ -23,8 +22,11 @@ window.onload=function(){
         .then(data => {
             console.log(data)
             usuario = data[0];
-            usuario_txt= document.createTextNode(usuario.Email)
-            input2.value=usuario.Email; 
+            //usuario_txt= document.createTextNode(usuario.Email)
+            inputNombre.value=usuario.Nombre; 
+            inputEmail.value=usuario.Email;
+            inputTelefono.value=usuario.Telefono;
+
         })
     }
 
@@ -32,9 +34,19 @@ function mostrar() {
     var formu = document.forms[0];
 
     var input1 = formu.campo1.value;
+    if(isNaN(input1)){
+        alert("Por favor debes introducir números como ID");
+        formu.campo1.focus();
+        return;
+    }
+    else if(input1 == ""){
+        alert("Por favor debes rellenar el campo ID");
+        formu.campo1.focus();
+        return;
+    }
     console.log(input1);
-
-    return input1;
+    llamada(input1)
+    
 }
 
 
