0c99918ff45bde3ef319bb1b08382c0fcbe84003
Juanjee24 <jsc0009@alu.medac.es>
2022-01-31 14:39:47 +0100

    terminado todos los endpoint falta dar estilos e implementarlos en el cliente con su funcionalidad

--- src/index.js
+++ src/index.js
@@ -174,8 +174,164 @@ var con = mysql.createConnection({
     });
   });
 
+  // Crear un nuevo usuario
+  
+  app.post("/crearUsuario", (req, res) => { 
 
 
+    const sql = "insert into usuarios set ?";
+
+    let usuario = {
+
+      Nombre:req.body.Nombre,
+      Telefono:req.body.Telefono,
+      Email:req.body.Email,
+      Contraseña: req.body.Contraseña
+
+    }
+
+    con.query(sql, usuario,err => {
+      if (err) throw err;
+      res.send("USUARIO CREADO");
+    });
+
+  });
+
+  //Eliminar un usuario
+
+  app.get("/borrarUsuario/:idUsuario", (req, res) => { 
+    const id = req.params.idUsuario;
+    const sql = "DELETE FROM usuarios WHERE ID_Usuario="+id;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+  
+      console.log("Result: " + JSON.stringify(result,null,2));
+      
+      res.json(result);
+    });
+    
+  });
+
+  // Modificar un vehículo
+  app.post("/modificarVehiculo", (req, res) => { 
+    const id = req.body.id_vehiculo;
+    const matricula = req.body.Matricula;
+    const marca = req.body.Marca;
+    const modelo = req.body.Modelo;
+    const anio_fabricacion = req.body.anio
+
+    
+    const sql = "UPDATE vehiculos SET Matricula = '"+matricula+"', Marca ='"+marca+"' , Modelo ='"+modelo+"', Año_fabricacion = '"+anio_fabricacion+"' where ID_Vehiculo ="+id;
+
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+
+      console.log("Result: " + JSON.stringify(result,null,2));
+
+      res.json(result);
+    });
+  });
+
+  // Crear un nuevo Vehículo
+  
+  app.post("/crearVehiculo", (req, res) => { 
+
+
+    const sql = "insert into vehiculos set ?";
+
+    let vehiculo = {
+
+      Matricula:req.body.Matricula,
+      Marca:req.body.Marca,
+      Modelo:req.body.Modelo,
+      Año_fabricacion: req.body.Año_fabricacion,
+      Id_usuario: req.body.Id_usuario
+
+    }
+
+    con.query(sql, vehiculo,err => {
+      if (err) throw err;
+      res.send("Vehiculo CREADO");
+    });
+
+  });
+
+   //Eliminar un usuario
+
+   app.get("/borrarVehiculo/:idVehiculo", (req, res) => { 
+    const id = req.params.idVehiculo;
+    const sql = "DELETE FROM vehiculos WHERE ID_Vehiculo="+id;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+  
+      console.log("Result: " + JSON.stringify(result,null,2));
+      
+      res.json(result);
+    });
+    
+  });
+
+   // Modificar un servicio
+   app.post("/modificarServicio", (req, res) => { 
+    const id = req.body.id_servicio;
+    const nombreServicio = req.body.nombre;
+    const precio = req.body.precio;
+    const tipo_servicio = req.body.tipo_servicio;
+    const descripcion = req.body.descripcion;
+    const fecha = req.body.fecha;
+
+    
+    const sql = "UPDATE servicios SET Nombre = '"+nombreServicio+"', Precio ='"+precio+"' , tipo_servicio ='"+tipo_servicio+"', descripcion = '"+descripcion+"', fecha='"+fecha+"' where ID_Servicio ="+id;
+
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+
+      console.log("Result: " + JSON.stringify(result,null,2));
+
+      res.json(result);
+    });
+  });
+
+
+   // Crear un nuevo Servicio
+  
+   app.post("/crearServicio", (req, res) => { 
+
+    
+    const sql = "insert into servicios set ?";
+
+    let servicio = {
+
+      Nombre:req.body.Nombre,
+      precio:req.body.precio,
+      tipo_servicio:req.body.tipo_servicio,
+      fecha: req.body.fecha,
+      ID_vehiculo: req.body.ID_vehiculo,
+      descripcion:req.body.descripcion
+
+    }
+
+    con.query(sql, servicio,err => {
+      if (err) throw err;
+      res.send("Servicio CREADO");
+    });
+
+  });
+
+   //Eliminar un servicio
+
+   app.get("/borrarServicio/:idServicio", (req, res) => { 
+    const id = req.params.idServicio;
+    const sql = "DELETE FROM servicios WHERE ID_Servicio="+id;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+  
+      console.log("Result: " + JSON.stringify(result,null,2));
+      
+      res.json(result);
+    });
+    
+  });
 
 
 //Iniciando el servidor, escuchando...
